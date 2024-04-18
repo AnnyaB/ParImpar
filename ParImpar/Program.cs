125cd0442981224be950a951b84bb0611a514fef
AnnyaB <anabordin2008@gmail.com>
2024-04-18 20:21:35 -0300

    Adicionar arquivos de projeto.

---
+++ ParImpar/Program.cs
@@ -0,0 +1,18 @@
+﻿// faça um programa que leia um número e informe se o número é par ou impar;
+
+int num;
+int numParImp;
+
+Console.WriteLine("Digite um número:");
+num = int.Parse(Console.ReadLine());
+
+numParImp = num % 2;
+
+if (numParImp == 0)
+{
+    Console.WriteLine("Esse número é par");
+}
+else
+{
+    Console.WriteLine("Esse número é impar");
+}
